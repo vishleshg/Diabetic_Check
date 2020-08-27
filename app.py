2ad79e241aac0fdc636993c3fc168076b1179a34
vishleshg <61452167+vishleshg@users.noreply.github.com>
2020-08-27 16:06:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,20 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from flask_cors import cross_origin
 
 
 app = Flask(__name__)
 model = pickle.load(open('diabetic.pkl', 'rb'))
 
 @app.route('/')
+@cross_origin()
 def home():
     return render_template('index.html')
 
 
 @app.route('/predict',methods=['POST'])
+@cross_origin()
 def predict():
 
     int_features =[float(x) for x in request.form.values()]
